470e28ae7f4dce825135ba8cb55e54bf53b743c3
ali-star <obanstar1@gmail.com>
2019-09-13 04:18:12 +0430

    update

---
+++ app/src/main/java/ir/siriusapps/ghalam/NotesFragment.kt
@@ -0,0 +1,12 @@
+package ir.siriusapps.ghalam
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+public class NotesFragment : Fragment(R.layout.fragment_notes) {
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
+
+}
---
+++ app/src/main/java/ir/siriusapps/ghalam/notes/NotesFragment.kt
@@ -0,0 +1,13 @@
+package ir.siriusapps.ghalam.notes
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import ir.siriusapps.ghalam.R
+
+class NotesFragment : Fragment(R.layout.fragment_notes) {
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
+
+}
